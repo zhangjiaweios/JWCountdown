adf7e08c83818c7e70c9ccfb456469bad3e9d584
zhangjiaweios <zhangjiaweios@gmail.com>
2016-08-14 14:06:37 +0800

    new project

---
+++ JWCountdown/JWCountdown/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JWCountdown
+//
+//  Created by zhangjiaweios on 16/8/14.
+//  Copyright © 2016年 zhangjiaweios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
